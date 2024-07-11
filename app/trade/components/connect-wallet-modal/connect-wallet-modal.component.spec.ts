1292fafc989ab4e56640dba9cd91bc00d929c446
rakermarket <support@raker.market>
2024-07-11 06:01:39 +0000

    Build from commit 3e048c787c84119168fb952b7e200701071b7b8d.

---
+++ app/trade/components/connect-wallet-modal/connect-wallet-modal.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConnectWalletModalComponent } from './connect-wallet-modal.component';
+
+describe('ConnectWalletModalComponent', () => {
+  let component: ConnectWalletModalComponent;
+  let fixture: ComponentFixture<ConnectWalletModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConnectWalletModalComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConnectWalletModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
